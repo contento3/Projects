8fe25c84c095426d8f0a279da4cafda7fb1c64ed
Aarij <aarij.siddiqui@live.com>
2012-02-13 22:08:54 +0500

    Group Dao and entity class.

--- core/src/main/java/com/contento3/cms/page/dao/PageDao.java
+++ core/src/main/java/com/contento3/cms/page/dao/PageDao.java
@@ -13,6 +13,7 @@ public interface PageDao extends GenericDao<Page,Integer> {
 	 * @param siteId id for a site
 	 * @return
 	 */
+	
 	Collection <Page> findPageBySiteId(Integer siteId,Integer pageNumber,Integer pageSize);
 
 	/**
