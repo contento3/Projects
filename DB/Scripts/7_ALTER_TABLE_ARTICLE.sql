51c765edfe5187c5abecbdeb7c38f1dd03e921c5
ssuet <ssuet@Arsalan-PC>
2012-02-23 02:42:46 +0500

    Updated

--- DB/Scripts/7_ALTER_TABLE_ARTICLE.sql
+++ DB/Scripts/7_ALTER_TABLE_ARTICLE.sql
@@ -1,7 +1,5 @@
-create table Article 
-(
-    Number_ID int(11) not null,
-     UUID varchar(45) not null,
-    primary key (Number_ID),
-    unique index (UUID)
-);
+ALTER TABLE article change ARTICLE_UUID ARTICLE_ID int(11);
+Alter Table Article ADD COLUMN ARTICLE_UUID varchar(45) UNIQUE KEY;
+
+
+
