9778377c73683b63b1b965b68bc2de80e55171a2
mehtabghani <mehtab.ghani@gmail.com>
2012-04-23 20:17:08 +0500

    alter article table

--- DB/Scripts/APRIL 2012/2_ALTER_ARTICLE_TABLE.sql
+++ DB/Scripts/APRIL 2012/2_ALTER_ARTICLE_TABLE.sql
@@ -7,3 +7,6 @@ alter table article add unique (ARTICLE_UUID);
 Alter table article  Modify date_created  date;
 Alter table article  Modify date_posted  date;
 Alter table article Modify body LONGTEXT;
+
+ALTER TABLE article ADD  LAST_UPDATED DATE AFTER DATE_POSTED;
+ALTER TABLE article ADD  EXPIRY_DATE DATE AFTER LAST_UPDATED;
