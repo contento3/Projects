66b986aefafc5ee94cf96c0226c65e8522f3dc33
nuketro0p3r <nuketro0p3r@gmail.com>
2012-12-23 20:48:04 +0500

    The DTO class for Document

---
+++ core/src/main/java/com/contento3/dam/document/dto/DocumentDto.java
@@ -0,0 +1,89 @@
+package com.contento3.dam.document.dto;
+
+import com.contento3.account.model.Account;
+import com.contento3.common.dto.Dto;
+import com.contento3.dam.document.model.DocumentType;
+import com.contento3.dam.storagetype.model.StorageType;
+
+public class DocumentDto extends Dto {
+	private Integer documentId;
+
+	private String documentUuid;
+
+	private String documentTitle;
+
+	private String url;
+
+	private StorageType storageType;
+
+	private byte[] documentContent;
+
+	private Account account;
+
+	private DocumentType documentType;
+	
+
+	public Integer getDocumentId() {
+		return documentId;
+	}
+
+	public void setDocumentId(final Integer documentId) {
+		this.documentId = documentId;
+	}
+
+	public String getDocumentUuid() {
+		return documentUuid;
+	}
+
+	public void setDocumentUuid(final String documentUuid) {
+		this.documentUuid = documentUuid;
+	}
+
+	public String getDocumentTitle() {
+		return documentTitle;
+	}
+
+	public void setDocumentTitle(final String documentTitle) {
+		this.documentTitle = documentTitle;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public StorageType getStorageType() {
+		return storageType;
+	}
+
+	public void setStorageType(final StorageType storageType) {
+		this.storageType = storageType;
+	}
+
+	public byte[] getDocumentContent() {
+		return documentContent;
+	}
+
+	public void setDocumentContent(final byte[] documentContent) {
+		this.documentContent = documentContent;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(final Account account) {
+		this.account = account;
+	}
+
+	public DocumentType getDocumentType() {
+		return documentType;
+	}
+
+	public void setDocumentType(final DocumentType documentType) {
+		this.documentType = documentType;
+	}
+}
