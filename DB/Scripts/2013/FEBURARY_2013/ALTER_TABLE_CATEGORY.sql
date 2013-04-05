e013b597af19c3fd1eb4007ae36e3776815ec100
nuketro0p3r <nuketro0p3r@gmail.com>
2013-04-05 23:06:50 +0500

    Fixed the bug of sites throwing an SQL exception when clicked caused by the absence of 'account_id' field inside the category table.

--- DB/Scripts/2013/FEBURARY_2013/ALTER_TABLE_CATEGORY.sql
+++ DB/Scripts/2013/FEBURARY_2013/ALTER_TABLE_CATEGORY.sql
@@ -1,4 +1,5 @@
 --- Change made to fix the the bug of sites not being listed once a page is added to it
 --- caused by the absense of 'account_id' attribute in the category table.
+
 ALTER TABLE CMS.CATEGORY ADD COLUMN ACCOUNT_ID INT NOT NULL DEFAULT 1;
 ALTER TABLE CMS.CATEGORY ADD CONSTRAINT FOREIGN KEY(ACCOUNT_ID) REFERENCES ACCOUNT(ACCOUNT_ID);
