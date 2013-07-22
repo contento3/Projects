d46bc5b7e9ac2b174d76103c6bca4e478488ca31
smn-khan <smn_khan92@yahoo.com>
2013-07-22 17:02:22 +0500

    to have a confirmation dialog while deleting page template.. a class YesNoDialog.java contains the confirm dialog stuff is created .. and is used in EntityDeleteClickListener.java class ..

--- cmsadmin/src/com/contento3/web/site/listener/EntityDeleteClickListener.java
+++ cmsadmin/src/com/contento3/web/site/listener/EntityDeleteClickListener.java
@@ -2,12 +2,17 @@ package com.contento3.web.site.listener;
 
 import com.contento3.common.exception.EntityCannotBeDeletedException;
 import com.contento3.common.service.Service;
+import com.contento3.web.site.PageTemplateTableBuilder;
+import com.vaadin.Application;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Window;
 
-public class EntityDeleteClickListener<T> implements ClickListener {
+
+
+public class EntityDeleteClickListener<T>  implements ClickListener {
 
 	private static final long serialVersionUID = 3126526402867446357L;
 
@@ -25,6 +30,12 @@ public class EntityDeleteClickListener<T> implements ClickListener {
 	 * Table that displays the entity listing.This is 
 	 * used here to delete the record from the displaying table.
 	 */
+	
+	private int decision;
+	// used in delete confirmation promt if true(2) then delete and if false(1) then leave
+	
+	private int response;
+	// check if user click yes or no on deleting template prompt
 	private final Table table;
 	
 	public final T getDtoToDelete() {
@@ -55,10 +66,40 @@ public class EntityDeleteClickListener<T> implements ClickListener {
 		this.service = service;
 	}
 
+
+
+	Window main = new Window();
+	//Window main = table.getApplication().getMainWindow();
+	
 	@Override
 	public void buttonClick(ClickEvent event) {
+		// TODO Auto-generated method stub
+		
+		//callConfirmDialog();
 		deleteEntity(dtoToDelete);
+		
 	}
+	
+/**	public String setDicisionValFalse() {
+		// TODO Auto-generated method stub
+		//decision = 0;
+		Window window = table.getApplication().getMainWindow();
+		String val = "false";
+		window.showNotification("canceled..");
+		//notWindow.getWindow();
+		return val;
+	}
+	public String setDicisionValTrue() {
+		// TODO Auto-generated method stub
+		//decision = 1;
+		String val1 = "true";
+		deleteEntity(dtoToDelete);
+		
+		//main.showNotification("Template is successfully unassigned");
+		
+		return val1;
+	}	**/
+				
 	
 	/**
 	 * Deletes the dto from the db and also the table.
@@ -66,13 +107,61 @@ public class EntityDeleteClickListener<T> implements ClickListener {
 	 */
 	protected void deleteEntity(final T dtoToDelete) {
 		final Object id = deleteLink.getData();
-		try {
-			service.delete(dtoToDelete);
-			table.removeItem(id);
-			table.setPageLength(table.getPageLength()-1);
-		} catch (EntityCannotBeDeletedException e) {
-			table.getApplication().getMainWindow().showNotification("Unable to delete entity."+e.getMessage());
-		}
-	}
+		final Window window = table.getApplication().getMainWindow();
+		window.addWindow(new YesNoDialog(
+				"Confirmation","Do you really want to delete template ?", "Yes", "No",
+				new YesNoDialog.YesNoDialogCallback() {
+					
+					@Override
+					public void response(boolean ok) {
+						// TODO Auto-generated method stub
+				if(ok)
+					//decision = 1;
+				//else decision = 0;
+				{
+					try {
+						service.delete(dtoToDelete);
+						table.removeItem(id);
+						table.setPageLength(table.getPageLength()-1);
+						//table.getApplication().getMainWindow().showNotification("Unable to delete entity."+e.getMessage());
+					window.showNotification("successfully unassigned");
+					} catch (EntityCannotBeDeletedException e) {
+						//table.getApplication().getMainWindow().showNotification("Unable to delete entity."+e.getMessage());
+					window.showNotification("Unable to delete entity");
+					}
+				}
+				else 
+					window.showNotification("canceled");
+					}
+				}
+				));
+		//if(decision == 1){
 		
-}	
+		//}
+		
+	}
+	
+
+	
+	/**public void callConfirmDialog(){
+	
+				// TODO Auto-generated method stub
+				main.addWindow(new YesNoDialog(
+						"sure","really want to delete ?","yes","no",
+						new YesNoDialog.YesNoDialogCallback() {
+							
+							@Override
+							public void response(boolean ok) {
+								// TODO Auto-generated method stub
+							main.showNotification(ok ? setDicisionValTrue() : setDicisionValFalse());	
+							}
+						}
+						)); 
+}**/
+
+				
+
+				
+	}
+	
+	
---
+++ cmsadmin/src/com/contento3/web/site/listener/YesNoDialog.java
@@ -0,0 +1,89 @@
+package com.contento3.web.site.listener;
+
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.VerticalLayout;
+
+
+
+public final class YesNoDialog extends Window implements Button.ClickListener{
+
+	//dialog height
+	private static final int CONFIRMATION_DIALOG_HIEGHT = 100;
+	//dialog width
+	private static final int CONFIRMATION_DIALOG_WIDTH = 320;
+	//constatnt of hundred percent used in dialog main layout width
+	private static final int ONE_HUNDRED_PERCENT = 100;
+	//serial version id
+	private static final long serialVersionUID = 1L;
+	//confirmation callback
+	private final YesNoDialogCallback callback;
+	//ok button
+	private final Button okButton;
+	//cancel button
+	private final Button cancelButton;
+	
+	//constructor for YesNoDialog 
+	//@param caption the dialog title
+	//@param question the dialog question
+	//@param ok and cancel button
+	//@param callback
+	
+	public YesNoDialog(final String caption, final String question, final String okLabel,
+			final String cancelLabel, final YesNoDialogCallback callback){
+		
+		super(caption);
+		setWidth(CONFIRMATION_DIALOG_WIDTH, YesNoDialog.UNITS_PIXELS);
+		setHeight(CONFIRMATION_DIALOG_HIEGHT, YesNoDialog.UNITS_PIXELS);
+		okButton = new Button(okLabel, this);
+		cancelButton = new Button(cancelLabel, this);
+		setModal(true);
+		
+		this.callback = callback;
+		
+		if(question != null)
+			addComponent(new Label(question));
+		
+		 
+	
+	//button layout
+	 HorizontalLayout layout = new HorizontalLayout();
+    layout.setSpacing(true);
+    layout.addComponent(okButton);
+    layout.addComponent(cancelButton);
+    addComponent(layout);
+    
+	
+	((VerticalLayout) getContent()).setHeight(ONE_HUNDRED_PERCENT,YesNoDialog.UNITS_PERCENTAGE);
+	
+	((VerticalLayout) getContent()).setComponentAlignment(layout,Alignment.BOTTOM_CENTER);
+}// constructor body ends
+	
+	
+	//event handler for button click
+	
+	public void buttonClick(ClickEvent event) {
+		// TODO Auto-generated method stub
+		
+		if(getParent() != null)
+			((Window) getParent()).removeWindow(this);
+		
+		callback.response(event.getSource() == okButton);
+	}
+	
+	//intrface yesnodialog callback
+	public interface YesNoDialogCallback {
+		
+		//user response
+		void response(boolean ok);
+	}
+
+	
+	
+
+	
+}
