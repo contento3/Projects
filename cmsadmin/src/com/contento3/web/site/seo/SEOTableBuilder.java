2cec0db89073290f8b66eb7c4d5a14cce36ab018
mehtabghani <mehtabghani@github.com>
2014-03-15 17:56:42 +0500

    Edit and Delete funtionality added for SEO settings screen.

--- cmsadmin/src/com/contento3/web/site/seo/SEOTableBuilder.java
+++ cmsadmin/src/com/contento3/web/site/seo/SEOTableBuilder.java
@@ -3,11 +3,14 @@ package com.contento3.web.site.seo;
 import com.contento3.cms.seo.dto.MetaTagDto;
 import com.contento3.common.dto.Dto;
 import com.contento3.web.common.helper.AbstractTableBuilder;
+import com.contento3.web.site.seo.listener.SEOListener;
 import com.vaadin.data.Container;
 import com.vaadin.data.Item;
 import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class SEOTableBuilder extends AbstractTableBuilder {
 
@@ -17,9 +20,19 @@ public class SEOTableBuilder extends AbstractTableBuilder {
 	private final static String TABLE_COLUMN_EDIT = "Edit";
 	private final static String TABLE_COLUMN_DELETE = "Delete";
 	
+	/**
+	 * SEO Listener
+	 */
+	private SEOListener seoListener;
+	
 	public SEOTableBuilder(Table table) {
 		super(table);
 	
+	}
+
+	public SEOTableBuilder(Table table, SEOListener listener) {
+		super(table);
+		this.seoListener = listener;
 	}
 
 	@Override
@@ -40,11 +53,27 @@ public class SEOTableBuilder extends AbstractTableBuilder {
 		Button edit = new Button(TABLE_COLUMN_EDIT);
 		edit.setData(tagDto.getMetaTagId());
 		edit.addStyleName("link");
+		edit.addClickListener(new ClickListener() {
+			
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void buttonClick(ClickEvent event) {
+				seoListener.editAttribute(tagDto);
+			}
+		});
 		item.getItemProperty(TABLE_COLUMN_EDIT).setValue(edit);
 		
 		Button delete = new Button(TABLE_COLUMN_DELETE);
 		delete.setData(tagDto.getMetaTagId());
 		delete.addStyleName("link");
+		delete.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void buttonClick(ClickEvent event) {
+				seoListener.deleteAttribute(tagDto);
+			}
+		});
 		item.getItemProperty(TABLE_COLUMN_DELETE).setValue(delete);
 	}
 
@@ -69,5 +98,5 @@ public class SEOTableBuilder extends AbstractTableBuilder {
 		Item item = container.addItem("-1");
 		item.getItemProperty(TABLE_COLUMN_ATTRIBUTE).setValue("No record found.");
 	}
-	
+
 }
