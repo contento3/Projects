022cca9edbb79d47b42eb9df4299b702eda95ce3
YawarJS <yawerjs@hotmail.com>
2014-03-15 19:58:01 -0700

    Sprint3 commit2 includes - slider dialect

--- sites/src/com/contento3/thymeleaf/dialect/slider/SliderProcessor.java
+++ sites/src/com/contento3/thymeleaf/dialect/slider/SliderProcessor.java
@@ -1,4 +1,4 @@
-package com.contento3.thymeleaf.dialect.article;
+package com.contento3.thymeleaf.dialect.slider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +15,11 @@ import org.thymeleaf.standard.expression.IStandardExpression;
 import org.thymeleaf.standard.expression.IStandardExpressionParser;
 import org.thymeleaf.standard.expression.StandardExpressions;
 import com.contento3.cms.article.service.ArticleService;
-public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor {
+public class SliderProcessor extends AbstractMarkupSubstitutionElementProcessor {
 
 	/**
-	 * Following are some of the usage scenarios of the <article:simple>
-	 * tag to display article content.
+	 * Following are some of the usage scenarios of the 
+	 * tag to display slider content.
 	 * The following examples gives you different 
 	 * scenarios of using the tag:
 	 * 
@@ -28,7 +28,7 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 	 * in html , so for using article dialect we will just provide template key
 	 * to fetch required template and all other code will be in those template
 	 *  
-	 * 	<article:simple templateKey="templateKey1" />
+	 * 	<slider templateKey="templateKey1" libraryId="libraryId" />
 	 * 	
 	 * if there is no template present for any key than it will show default error template 
 	 * defined in getMarkupSubstitute method.  
@@ -36,8 +36,8 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 	
 	private ArticleService articleService;
 
-	protected ArticleProcessor() {
-		super("simple");
+	protected SliderProcessor() {
+		super("");
 	}
 
 	@Override
@@ -45,14 +45,13 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 			final Element element) {
 		final String defaultTemplateKey = "default"; 	// will be used if required template is missing
 		String templateKey = parseString(arguments,element,"templateKey");
-		String categoryId  = parseString(arguments,element,"categoryId");
-		String templateId  = parseString(arguments,element,"templateId");
+		String libraryId   = parseString(arguments,element,"libraryId");
+		
 		if(templateKey == null){
 			templateKey = defaultTemplateKey;
 		}
 
 		final List<Node> nodes = new ArrayList<Node>();
-		
 		/*
 		 * Below line will never return null because
 		 * on empty result it will return default error template
@@ -60,8 +59,7 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 		 */
 		Template template = arguments.getTemplateRepository().getTemplate(new TemplateProcessingParameters(arguments.getConfiguration(), "/template_key/"+templateKey, arguments.getContext()));
 		Document doc = template.getDocument();
-		Element htmlToAdd = doc.getFirstElementChild() ;
-		
+		Element htmlToAdd = doc.getFirstElementChild() ; 
 		if(htmlToAdd == null)
 	    {
 			template = arguments.getTemplateRepository().getTemplate(new TemplateProcessingParameters(arguments.getConfiguration(), "/template/site1/"+defaultTemplateKey, arguments.getContext()));
@@ -70,21 +68,13 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 			nodes.add((Node)htmlToAdd);
 	    }
 		else{
-			htmlToAdd.setNodeLocalVariable("catId", categoryId);
-			htmlToAdd.setNodeLocalVariable("templateId", templateId);
+			htmlToAdd.setNodeLocalVariable("libraryId", libraryId);
 			nodes.add((Node)htmlToAdd);
 		}
 
 	    return nodes;
 	   }
 
-	/**
-	 * 
-	 * @param arguments
-	 * @param element
-	 * @param attributeName
-	 * @return
-	 */
 	private String parseString(final Arguments arguments,final Element element,final String attributeName){
 		String parsedAttribute = null;
 		String attributeValue = element.getAttributeValue(attributeName);
@@ -95,13 +85,6 @@ public class ArticleProcessor extends AbstractMarkupSubstitutionElementProcessor
 		return parsedAttribute;
 	}
 		
-	/**
-	 * 
-	 * @param arguments
-	 * @param element
-	 * @param attributeValue
-	 * @return
-	 */
 	private IStandardExpression buildExpression(final Arguments arguments,final Element element,final String attributeValue){
         final Configuration configuration = arguments.getConfiguration();
  		final IStandardExpressionParser parser = StandardExpressions.getExpressionParser(configuration);
