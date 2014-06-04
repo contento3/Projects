a217eb514c7d881d5dddef78e9acd1d0f5884902
Mehtab <mehtabghani@github.com>
2014-06-04 17:03:27 +0500

    Document download task has been completed.

--- cmsadmin/src/com/contento3/web/content/document/DocumentTableBuilder.java
+++ cmsadmin/src/com/contento3/web/content/document/DocumentTableBuilder.java
@@ -1,5 +1,8 @@
 package com.contento3.web.content.document;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import com.contento3.common.dto.Dto;
 import com.contento3.dam.document.dto.DocumentDto;
 import com.contento3.dam.document.service.DocumentService;
@@ -9,7 +12,10 @@ import com.contento3.web.content.document.listener.DocumentFormBuilderListner;
 import com.contento3.web.helper.SpringContextHelper;
 import com.vaadin.data.Container;
 import com.vaadin.data.Item;
+import com.vaadin.server.FileDownloader;
 import com.vaadin.server.Sizeable.Unit;
+import com.vaadin.server.StreamResource;
+import com.vaadin.server.StreamResource.StreamSource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.Table;
@@ -57,7 +63,8 @@ public class DocumentTableBuilder extends AbstractTableBuilder {
 	 */
 	@Override
 	public void assignDataToTable(final Dto dto, final Table documentTable, final Container documentContainer) {
-		DocumentDto documentDto = (DocumentDto) dto;
+		
+		final DocumentDto documentDto = (DocumentDto) dto;
 		Integer id = documentDto.getDocumentId();
 		Item item = documentContainer.addItem(id);
 		item.getItemProperty("documents").setValue(documentDto.getDocumentTitle());
@@ -70,8 +77,13 @@ public class DocumentTableBuilder extends AbstractTableBuilder {
 		item.getItemProperty("delete").setValue(deleteLink);
 		deleteLink.addClickListener(new DocumentDeleteListener(documentDto, documentService, deleteLink, documentTable));
 	
-		Button btnDownload = createLinkButton(BUTTON_NAME_DOWNLOAD, id);
-		//btnDownload.addClickListener(listener);
+		final Button btnDownload = createLinkButton(BUTTON_NAME_DOWNLOAD, id);
+
+		//Download document
+		StreamResource res =  createResource(documentDto);
+		FileDownloader fd = new FileDownloader(res);
+		fd.extend(btnDownload);
+		
 		item.getItemProperty(BUTTON_NAME_DOWNLOAD).setValue(btnDownload);
 	}
 		
@@ -107,4 +119,33 @@ public class DocumentTableBuilder extends AbstractTableBuilder {
 		item.getItemProperty("documents").setValue("No record found.");
 	}
 	
+	/**
+	 * Get full file name
+	 * @param dto
+	 * @return
+	 */
+	private String getFileNameWithExtension(final DocumentDto dto) {
+		
+		return dto.getDocumentTitle()+ "." + dto.getDocumentTypeDto().getName().toLowerCase();
+	}
+	
+	/**
+	 * Prepare resource to download.
+	 * @param dto
+	 * @return
+	 */
+	private StreamResource createResource(final DocumentDto dto) {
+        return new StreamResource(new StreamSource() {
+            @Override
+            public InputStream getStream() {
+
+                try {
+                	return new ByteArrayInputStream(dto.getDocumentContent());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }, getFileNameWithExtension(dto));
+    }
 }
